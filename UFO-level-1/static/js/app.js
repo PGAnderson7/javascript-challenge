8aa413d534b5ca7f595164c32746e5466f7f4600
Peter Anderson <peter.g.anderson1@gmail.com>
2021-06-24 18:37:15 -0500

    started

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,22 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Use d3 to update each cell's text with data values (`date/time`, `city`, `state`, `country`, `shape`, and `comment`)
+function makeTable(tableData){
+    var tbody = d3.select(".table").select("tbody");
+    tbody.html("");
+    tableData.forEach((x) => {
+        var row = tbody.append("tr");
+        Object.entries(x).forEach(([key, value]) => {
+            // Append a cell to the row for each value in the report object
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
+
+// Make table in html 
+makeTable(tableData);
+
+// Getting a reference to the button on the page with the id property set to `filter-btn`
+var button = d3.select("#filter-btn");
